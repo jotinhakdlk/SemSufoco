4ca839c3f150f6c238dc5f2e3bb78c0bffcd6da3
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-08-04 11:47:20 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,7 @@
+nomeUsuario = document.getElementById("nomeUsuario").value;
+emailUsuario = document.getElementById("emailUsuario").value;
+rendaMensal = document.getElementById("rendaMensal").value;
+
+function CadastroConta(){
+
+};
