53245087ad666c5b73903a339e8a1ea0cec42eeb
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-08-06 11:10:54 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,7 +1,11 @@
-nomeUsuario = document.getElementById("nomeUsuario").value;
-emailUsuario = document.getElementById("emailUsuario").value;
-rendaMensal = document.getElementById("rendaMensal").value;
-
 function CadastroConta(){
+    nomeUsuario = document.getElementById("nomeUsuario").value;
+    emailUsuario = document.getElementById("emailUsuario").value;
+    rendaMensal = document.getElementById("rendaMensal").value;
 
+    if (nomeUsuario != "" && emailUsuario != "" && rendaMensal > 0){
+        document.getElementById("TelaLogin").style.display = "none"
+    } else{
+        document.getElementById("aviso").innerHTML = "<strong>Preencha todos os campos corretamente!</strong>"
+    }
 };
